10ca2a9d64ad99c1197d4368c95802b4c7e60ef7
Nicolas Jeremías <nicolasje@lagash.com>
2017-05-03 21:01:20 -0300

    First commit

---
+++ contact-list/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { ContactListPage } from './app.po';
+
+describe('contact-list App', () => {
+  let page: ContactListPage;
+
+  beforeEach(() => {
+    page = new ContactListPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
